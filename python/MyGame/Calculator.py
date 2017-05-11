0e2567913556780bee2d81304210644a6b294932
zhenyashumel <shumel02@mail.ru>
2017-05-11 21:21:38 +0300

    Update Calculator.py

--- python/MyGame/Calculator.py
+++ python/MyGame/Calculator.py
@@ -1,8 +1,12 @@
+#calculator  2017 © ZhenyaProduction,Inc.
 from tkinter import *
 from math import *
 
 
 
+
+
+#класс всех переменных
 class Per():
     first=0.0   #число для действий
     second=0.0  #число для действий
@@ -10,55 +14,56 @@ class Per():
     second_active=False #переключатель для второго числа
     op=" "     #операция
     last_value=0  #последнее число(нужно для удаления)
-    have_tochka=False
-    first_tochka = 0
-    first_kol_tochka=1
-    second_tochka=0
-    second_kol_tochka=1
-    clear_index=0
-    clear_od=1
-    flag_active_second=False
-    sec=False
-    cant_toch=False
-    cant_op=False
-    have_first_tochka=False
-    have_second_tochka=False
+    have_tochka=False #наличие точки
+    first_tochka = 0 #число после запятой первого числа
+    first_kol_tochka=1 #делитель точки первого числа
+    second_tochka=0 #число после запятой второго числа
+    second_kol_tochka=1 #делитель точки второго числа
+    clear_index=0 #индекс для очистки поля
+    clear_od=1 #второй индекс для очистки поля
+    flag_active_second=False #нужно для очистки знаков операций
+    sec=False #что-то там
+    cant_toch=False #возможность ввода точки
+    cant_op=False #возможность ввода операций
+    have_first_tochka=False #наличие точки первого числа
+    have_second_tochka=False #наличие точки второго числа
+    temp=0.0#переменная для хранение числа в память
+
 
 
 #кнопки чисел
 class Buttons_numbers():
-
+    #инициализация кнопки и её расположение в окне
     def __init__(self,text,Row,Column,Value):
-        self.but = Button(root,
+        self.but = Button(root,bg="Yellow",
                           text=text,
                           width=6,
-                          height=2)
-
-
+                          height=2
+                          )
         self.value = int(Value)
         self.but.bind("<Button-1>",self.press)
-
         self.but.grid(row=Row, column=Column)
 
 
 
     #функция нажатия на кнопку числа
     def press(self, event):
-
+        #случай , если нет точки
         if Per.have_tochka==False:
-
+            #и нету второй точки
             if Per.second_active==False:
 
                 Per.first =float(Per.first*10+self.value)
                 tex.insert(END, self.value)
                 Per.last_value=self.value
                 Per.clear_index +=1
-
+            #если есть вторая точка
             else:
                 Per.second = float(Per.second * 10 + self.value)
                 tex.insert(END, self.value)
                 Per.clear_index += 1
                 Per.cant_toch = False
+        #теперь точка есть
         else:
             if Per.second_active==False:
                 tex.insert(END, self.value)
@@ -70,19 +75,19 @@ class Buttons_numbers():
                 Per.second_kol_tochka *= 10
 
 
-
+#кнопки операций
 class Buttons_operations(Per):
-
+    #инициализация и расположение кнопок
     def __init__(self, Text, Row, Column):
         if Text != "=":
-            self.but = Button(root,
+            self.but = Button(root,bg="light Blue",
                                 text=Text,
                                 width=6,
                                 height=2)
         else:
-            self.but = Button(root,
+            self.but = Button(root,bg="Red",
                                 text=Text,
-                                width=41,
+                                width=50,
                                 height=2,
                                 )
         self.text = Text
@@ -90,12 +95,13 @@ class Buttons_operations(Per):
         if Text != "=":
             self.but.grid(row=Row, column=Column)
         else:
-            self.but.grid(row=6, column=0, columnspan=7)
+            self.but.grid(row=6, column=0, columnspan=8)
 
+    #функция нажатия
     def press(self, event):
         Per.clear_index += 1
 
-
+        #нажатие на плюсик
         if self.text == "+":
             if Per.cant_op==False:
                 Per.op = "+"
@@ -107,9 +113,7 @@ class Buttons_operations(Per):
                 Per.clear_index -= 1
 
 
-
-
-
+        # нажатие на минусик
         if self.text == "-":
             if Per.cant_op == False:
                 Per.op = "-"
@@ -120,7 +124,7 @@ class Buttons_operations(Per):
             else:
                 Per.clear_index -= 1
 
-
+        # нажатие на деление
         if self.text == "/":
             if Per.cant_op == False:
                 Per.op = "/"
@@ -131,7 +135,7 @@ class Buttons_operations(Per):
             else:
                 Per.clear_index -= 1
 
-
+        # нажатие на умножалку
         if self.text == "*":
             if Per.cant_op == False:
                 Per.op = "*"
@@ -142,8 +146,7 @@ class Buttons_operations(Per):
             else:
                 Per.clear_index -= 1
 
-
-
+        # нажатие на остаток от деления
         if self.text == "%":
             if Per.cant_op == False:
                 Per.op = "%"
@@ -154,6 +157,7 @@ class Buttons_operations(Per):
             else:
                 Per.clear_index -= 1
 
+        # нажатие на дробь
         if self.text == "1/x":
             if Per.cant_op == False:
                 tex.insert(END, "=")
@@ -163,7 +167,7 @@ class Buttons_operations(Per):
             else:
                 Per.clear_index -= 1
 
-
+        # нажатие на степень
         if self.text == "x^y":
             if Per.cant_op == False:
                 Per.op = "x^y"
@@ -174,6 +178,7 @@ class Buttons_operations(Per):
             else:
                 Per.clear_index -= 1
 
+        # нажатие на корень
         if self.text == "√":
             if Per.cant_op == False:
                 tex.insert(END, "=")
@@ -182,6 +187,54 @@ class Buttons_operations(Per):
                 Per.cant_op = True
             else:
                 Per.clear_index -= 1
+
+        # нажатие на синус
+        if self.text == "sin":
+            if Per.cant_op == False:
+                if Per.first!=30:
+                    tex.insert(END, "=")
+                    tex.insert(END, sin(Per.first))
+                    Per.have_tochka = False
+                    Per.cant_op = True
+                else:
+                    tex.insert(END, "=")
+                    tex.insert(END, "0.5")
+
+            else:
+                Per.clear_index -= 1
+
+        # нажатие на косинус
+        if self.text == "cos":
+            if Per.cant_op == False:
+                if Per.first!=30:
+                    tex.insert(END, "=")
+                    tex.insert(END,cos(Per.first))
+                    Per.have_tochka = False
+                    Per.cant_op = True
+                else:
+                    tex.insert(END, "=")
+                    tex.insert(END, "0.86602540378")
+
+            else:
+                Per.clear_index -= 1
+
+        #добавление в память(работает с первым числом)
+        if self.text == "M+":
+            Per.temp=Per.first
+            if Per.first>0:
+                lab1 = Label(root, text="\t                Вычислятор       M+    ", font="15").grid(row=0, column=0, columnspan=7)
+                Per.temp = Per.first
+            else:
+
+                lab1 = Label(root, text="\t               Вычислятор   error!!!", font="15").grid(row=0,
+                                                                                                     column=0,
+                                                                                                     columnspan=7)
+
+        # использование из памяти
+        if self.text == "MR":
+            tex.insert(END, Per.temp)
+            Per.second=Per.temp
+
 
         #ТООООООЧКААААААА
         if self.text == ".":
@@ -241,15 +294,8 @@ class Buttons_operations(Per):
                     Per.second_kol_tochka/=10
 
 
-
-
-
-
-
-
-
         #нажатие равно
-        if self.text == "=":
+        if self.text == "=" or self.text=="MR":
             Per.second+=Per.second_tochka/Per.second_kol_tochka
             Per.first+=Per.first_tochka/Per.first_kol_tochka
             tex.insert(END, "=")
@@ -268,7 +314,7 @@ class Buttons_operations(Per):
                 tex.insert(END, float(float(Per.first) %  float(Per.second)))
             if Per.op == "x^y":
                 tex.insert(END, float(float(Per.first) ** float(Per.second)))
-            print(Per.first,Per.second ) #на случай , если будут проблемы с вычеслениями - расскоментить
+           # print(Per.first,Per.second ) #на случай , если будут проблемы с вычеслениями - расскоментить
 
 
 
@@ -277,9 +323,11 @@ root=Tk()
 root.title("Calculator")
 
 
-lab=Label(root,text="\tВычислятор",font="15").grid(row=0, column=0,columnspan=6)
-tex = Entry(root,width=48)
+#надпись
+lab=Label(root,text="\tВычислятор",font="15").grid(row=0, column=0,columnspan=7)
+tex = Entry(root,width=60)
 
+#кнопки операций
 but_plus = Buttons_operations("+", 2, 5)
 but_proc = Buttons_operations("%", 2, 6)
 but_min = Buttons_operations("-", 3, 5)
@@ -288,11 +336,16 @@ but_mnozh = Buttons_operations("*", 4, 5)
 but_step = Buttons_operations("x^y", 4, 6)
 but_del = Buttons_operations("/", 5, 5)
 but_kor = Buttons_operations("√", 5, 6)
+but_sin = Buttons_operations("sin", 2, 7)
+but_cos = Buttons_operations("cos", 3, 7)
+but_cos = Buttons_operations("M+", 4, 7)
+but_cos = Buttons_operations("MR", 5, 7)
 but_ravn = Buttons_operations("=",6,0)
 but_toch = Buttons_operations(".", 5, 0)
 but_delete = Buttons_operations("←", 5, 2)
 
 
+#кнопки чисел
 but1 = Buttons_numbers("1", 2, 0, 1)
 but2 = Buttons_numbers("2", 2, 1, 2)
 but3 = Buttons_numbers("3", 2, 2, 3)
@@ -306,5 +359,5 @@ but0 = Buttons_numbers("0", 5, 1, 0)
 
 
 
-tex.grid(row=1, column=0,columnspan=7)
+tex.grid(row=1, column=0,columnspan=8)
 root.mainloop()
