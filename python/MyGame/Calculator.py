596d80244bc05ee5697cabbc8aba3bd5c07f256d
zhenyashumel <shumel02@mail.ru>
2017-05-14 11:57:07 +0200

    Update Calculator.py

--- python/MyGame/Calculator.py
+++ python/MyGame/Calculator.py
@@ -1,4 +1,4 @@
-#calculator  2017 © ZhenyaProduction,Inc.
+#calculator  © 2017 ZhenyaProduction, Inc.
 from tkinter import *
 from math import *
 
@@ -10,7 +10,7 @@ from math import *
 class Per():
     first=0.0   #число для действий
     second=0.0  #число для действий
-    otv=0       #ответ
+    otv=0.0      #ответ
     second_active=False #переключатель для второго числа
     op=" "     #операция
     last_value=0  #последнее число(нужно для удаления)
@@ -28,14 +28,39 @@ class Per():
     have_first_tochka=False #наличие точки первого числа
     have_second_tochka=False #наличие точки второго числа
     temp=0.0#переменная для хранение числа в память
+    clear_all=False #переменная , которая удаляпет всё после нажатия равно
+    temp_otv=False
 
+
+
+def Clear_all():
+    tex.delete(0, END)
+    Per.first = 0
+    Per.second = 0.0
+    Per.otv = 0
+    Per.second_active = False
+    Per.op = " "
+    Per.last_value = 0
+    Per.have_tochka = False
+    Per.first_tochka = 0
+    Per.first_kol_tochka = 1
+    Per.second_tochka = 0
+    Per.second_kol_tochka = 1
+    Per.clear_index = 0
+    Per.clear_od = 1
+    Per.flag_active_second = False
+    Per.sec = False
+    Per.cant_toch = False
+    Per.cant_op = False
+    Per.have_first_tochka = False
+    Per.have_second_tochka = False
 
 
 #кнопки чисел
 class Buttons_numbers():
     #инициализация кнопки и её расположение в окне
     def __init__(self,text,Row,Column,Value):
-        self.but = Button(root,bg="Yellow",
+        self.but = Button(root,bg="LawnGreen",
                           text=text,
                           width=6,
                           height=2
@@ -48,6 +73,9 @@ class Buttons_numbers():
 
     #функция нажатия на кнопку числа
     def press(self, event):
+        if Per.clear_all==True:
+            Clear_all()
+            Per.clear_all = False
         #случай , если нет точки
         if Per.have_tochka==False:
             #и нету второй точки
@@ -80,14 +108,14 @@ class Buttons_operations(Per):
     #инициализация и расположение кнопок
     def __init__(self, Text, Row, Column):
         if Text != "=":
-            self.but = Button(root,bg="light Blue",
+            self.but = Button(root,bg="DarkSlateGray1",
                                 text=Text,
                                 width=6,
                                 height=2)
         else:
-            self.but = Button(root,bg="Red",
+            self.but = Button(root,bg="MediumAquamarine",
                                 text=Text,
-                                width=50,
+                                width=51,
                                 height=2,
                                 )
         self.text = Text
@@ -100,6 +128,7 @@ class Buttons_operations(Per):
     #функция нажатия
     def press(self, event):
         Per.clear_index += 1
+
 
         #нажатие на плюсик
         if self.text == "+":
@@ -190,50 +219,74 @@ class Buttons_operations(Per):
 
         # нажатие на синус
         if self.text == "sin":
+            Per.clear_all = True
+            Per.temp_otv = True
             if Per.cant_op == False:
                 if Per.first!=30:
                     tex.insert(END, "=")
                     tex.insert(END, sin(Per.first))
+                    Per.otv=sin(Per.first)
                     Per.have_tochka = False
                     Per.cant_op = True
                 else:
                     tex.insert(END, "=")
                     tex.insert(END, "0.5")
+                    Per.otv=sin(Per.first)
 
             else:
                 Per.clear_index -= 1
 
         # нажатие на косинус
         if self.text == "cos":
+
             if Per.cant_op == False:
+                Per.clear_all = True
+                Per.temp_otv = True
                 if Per.first!=30:
                     tex.insert(END, "=")
                     tex.insert(END,cos(Per.first))
+                    Per.otv=cos(Per.first)
                     Per.have_tochka = False
                     Per.cant_op = True
                 else:
                     tex.insert(END, "=")
                     tex.insert(END, "0.86602540378")
+                    Per.otv=0.86602540378
 
             else:
                 Per.clear_index -= 1
 
         #добавление в память(работает с первым числом)
         if self.text == "M+":
-            Per.temp=Per.first
-            if Per.first>0:
-                lab1 = Label(root, text="\t                Вычислятор       M+    ", font="15").grid(row=0, column=0, columnspan=7)
-                Per.temp = Per.first
+            if Per.temp_otv==True:
+                Per.temp=Per.otv
+                lab1 = Label(root, text="M+", font="Arial 15 bold", fg="DarkOrange").grid(row=0, column=6)
             else:
+                if Per.second_active==True:
+                    Per.temp=Per.second+Per.second_tochka/Per.second_kol_tochka
+                    lab1 = Label(root, text="M+", font="Arial 15 bold", fg="DarkOrange").grid(row=0, column=6)
+                else:
+                    if Per.first==0.0 and Per.first_tochka==0:
+                        lab3 = Label(root, text="error!!!", font="Arial 15 bold", fg="DarkOrange").grid(row=0, column=6,
+                                                                                                        columnspan=2)
+                    else:
+                        Per.temp=Per.first+Per.first_tochka/Per.first_kol_tochka
+                        lab1 = Label(root, text="M+", font="Arial 15 bold", fg="DarkOrange").grid(row=0, column=6)
 
-                lab1 = Label(root, text="\t               Вычислятор   error!!!", font="15").grid(row=0,
-                                                                                                     column=0,
-                                                                                                     columnspan=7)
+
+
 
         # использование из памяти
         if self.text == "MR":
-            tex.insert(END, Per.temp)
-            Per.second=Per.temp
+            if Per.temp!=0:
+                if Per.clear_all == True:
+                    Clear_all()
+                    Per.clear_all = False
+                tex.insert(END, Per.temp)
+                if Per.second_active==False:
+                    Per.first=Per.temp
+                else:
+                    Per.second=Per.temp
 
 
         #ТООООООЧКААААААА
@@ -256,75 +309,107 @@ class Buttons_operations(Per):
 
         #очистка
         if self.text == "←":
-
-            if Per.have_tochka==False:
-                Per.clear_index = int(Per.clear_index - 1)
-                tex.delete(Per.clear_index - Per.clear_od, Per.clear_index)
-                Per.clear_od += 1
-
-
-                if Per.second_active==False:
-
-                    if Per.first>9:
-                        Per.first=int(Per.first/10)
-                    else:
-                        Per.first=0
-                if Per.flag_active_second==True:
-                    Per.flag_active_second = True
-                    Per.active_second=False
-
-                if Per.sec==False:
-
-                    if Per.second_active==True:
-                        if Per.second>9:
-                            Per.second=int(Per.second/10)
-                        elif Per.second<10:
-
-                            Per.second = 0
-                            Per.sec=True
-                            Per.flag_active_second=True
-
+            if Per.clear_all == True:
+                Clear_all()
+                Per.clear_all = False
             else:
+                Per.cant_op=False
 
-                Per.clear_index = int(Per.clear_index - 1)
-                tex.delete(Per.clear_index - Per.clear_od, Per.clear_index)
-                Per.clear_od += 1
-                if Per.have_second_tochka==True:
-                    Per.second_tochka/=10
-                    Per.second_kol_tochka/=10
+
+                if Per.have_tochka==False:
+                    Per.clear_index = int(Per.clear_index - 1)
+                    tex.delete(Per.clear_index - Per.clear_od, Per.clear_index)
+                    Per.clear_od += 1
+
+
+                    if Per.second_active==False:
+
+                        if Per.first>9:
+                            Per.first=int(Per.first/10)
+                        else:
+                            Per.first=0
+                    if Per.flag_active_second==True:
+                        Per.flag_active_second = True
+                        Per.active_second=False
+
+                    if Per.sec==False:
+
+                        if Per.second_active==True:
+                            if Per.second>9:
+                                Per.second=int(Per.second/10)
+                            elif Per.second<10:
+
+                                Per.second = 0
+                                Per.sec=True
+                                Per.flag_active_second=True
+
+                else:
+
+                    Per.clear_index = int(Per.clear_index - 1)
+                    tex.delete(Per.clear_index - Per.clear_od, Per.clear_index)
+                    Per.clear_od += 1
+                    if Per.have_second_tochka==True:
+                        Per.second_tochka/=10
+                        Per.second_kol_tochka/=10
 
 
         #нажатие равно
-        if self.text == "=" or self.text=="MR":
-            Per.second+=Per.second_tochka/Per.second_kol_tochka
-            Per.first+=Per.first_tochka/Per.first_kol_tochka
-            tex.insert(END, "=")
-            if Per.op=="+":
-                tex.insert(END,float(float(Per.first)+float(Per.second)))
 
-            if Per.op == "-":
-                tex.insert(END, float(float(Per.first) - float(Per.second)))
+        if self.text == "=":
+            if Per.clear_all == False:
 
-            if Per.op == "/":
-                tex.insert(END, float(float(Per.first) / float(Per.second)))
+                Per.clear_all=True
+                Per.temp_otv=True
 
-            if Per.op == "*":
-                tex.insert(END, float(float(Per.first) * float(Per.second)))
-            if Per.op == "%":
-                tex.insert(END, float(float(Per.first) %  float(Per.second)))
-            if Per.op == "x^y":
-                tex.insert(END, float(float(Per.first) ** float(Per.second)))
-           # print(Per.first,Per.second ) #на случай , если будут проблемы с вычеслениями - расскоментить
+                Per.second+=Per.second_tochka/Per.second_kol_tochka
+                Per.first+=Per.first_tochka/Per.first_kol_tochka
+                tex.insert(END, "=")
+                if Per.op=="+":
+                    tex.insert(END,float(float(Per.first)+float(Per.second)))
+                    Per.otv=float(float(Per.first)+float(Per.second))
 
+                if Per.op == "-":
+                    tex.insert(END, float(float(Per.first) - float(Per.second)))
+                    Per.otv = float(float(Per.first) - float(Per.second))
 
+                if Per.op == "/":
+                    tex.insert(END, float(float(Per.first) / float(Per.second)))
+                    Per.otv = float(float(Per.first) / float(Per.second))
 
+                if Per.op == "*":
+                    tex.insert(END, float(float(Per.first) * float(Per.second)))
+                    Per.otv = float(float(Per.first) * float(Per.second))
+                if Per.op == "%":
+                    tex.insert(END, float(float(Per.first) %  float(Per.second)))
+                    Per.otv = float(float(Per.first) % float(Per.second))
+                if Per.op == "x^y":
+                    tex.insert(END, float(float(Per.first) ** float(Per.second)))
+                    Per.otv = float(float(Per.first) ** float(Per.second))
+               # print(Per.first,Per.second ) #на случай , если будут проблемы с вычеслениями - расскоментить
+#функция нажатия на кнопочку меню about
+def about():
+    win = Toplevel(root)
+    win.title("About Calulator")
+    lab = Label(win, text="Это просто калькулятор.\n Ничего особенного , тупо калькулятор\n"
+                          "  Если он работает , то вам очень повезло , поскольку писал его я  "+
+                "\n© 2017 ZhenyaProduction, Inc.",font="Arial 15 bold")
+    lab.pack()
+
+#функция нажатия на кнопочку меню exit
+def close_win():
+    root.destroy()
 #main
 root=Tk()
+root.resizable(0,0)
 root.title("Calculator")
+def New():
+    Clear_all()
+    Per.clear_all = False
+
 
 
 #надпись
-lab=Label(root,text="\tВычислятор",font="15").grid(row=0, column=0,columnspan=7)
+lab=Label(root,text="          Вычислятор",font="Arial 15 bold",fg="DarkOrange").grid(row=0, column=0,columnspan=7)
 tex = Entry(root,width=60)
 
 #кнопки операций
@@ -338,8 +423,8 @@ but_del = Buttons_operations("/", 5, 5)
 but_kor = Buttons_operations("√", 5, 6)
 but_sin = Buttons_operations("sin", 2, 7)
 but_cos = Buttons_operations("cos", 3, 7)
-but_cos = Buttons_operations("M+", 4, 7)
-but_cos = Buttons_operations("MR", 5, 7)
+but_M = Buttons_operations("M+", 4, 7)
+but_MR = Buttons_operations("MR", 5, 7)
 but_ravn = Buttons_operations("=",6,0)
 but_toch = Buttons_operations(".", 5, 0)
 but_delete = Buttons_operations("←", 5, 2)
@@ -357,6 +442,15 @@ but8 = Buttons_numbers("8", 4, 1, 8)
 but9 = Buttons_numbers("9", 4, 2, 9)
 but0 = Buttons_numbers("0", 5, 1, 0)
 
+
+m = Menu(root) #создается объект Меню на главном окне
+root.config(menu=m)
+fm = Menu(m) #создается пункт меню с размещением на основном меню (m)
+m.add_cascade(label="File",menu=fm) #пункту располагается на основном меню (m)
+#формируется список команд пункта меню
+fm.add_command(label="New",command=New)
+fm.add_command(label="About",command=about)
+fm.add_command(label="Exit",command=close_win)
 
 
 tex.grid(row=1, column=0,columnspan=8)
