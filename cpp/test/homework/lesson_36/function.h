edb6521f42d8f4d770ac8b2cbe11b4ef03f9dcb5
zhenyashumel <shumel02@mail.ru>
2017-05-21 14:11:12 +0200

    Update function.h

--- cpp/test/homework/lesson_36/function.h
+++ cpp/test/homework/lesson_36/function.h
@@ -7,11 +7,13 @@ using namespace std;
 #define sqr(x) (x*x)
 #define step(a,b) (pow(a,b))
 #define iseven(a,b) (a%2==0?"Even":"Odd")
+#define Init_double Init
+#define Init_int Init
+#define Init_char Init
 
-
-void Init(double mass[], int size);
-void Init(int mass[], int size);
-void Init(char mass[], int size);
+void Init_double(double mass[], int size);
+void Init_int(int mass[], int size);
+void Init_char(char mass[], int size);
 
 
 void Print(double mass[], int size);
@@ -29,11 +31,11 @@ int Max(int mass[], int size);
 char Max(char mass[], int size);
 
 
-void Sort(double mass[], int size);
-void Sort(int mass[], int size);
-void Sort(char mass[], int size);
+void Sort(double *A, int size);
+void Sort(int *A, int size);
+void Sort(char *A, int size);
 
 
-void Red(double mass[], int size, int index);
-void Red(int mass[], int size, int index);
-void Red(char mass[], int size, int index);
+void Red(double *mass, int size, int index, int value);
+void Red(int *mass, int size, int index, int value);
+void Red(char *mass, int size, int index, int value);
