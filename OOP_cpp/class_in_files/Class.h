c4e469e4ffdbd8df6430eb75f0f1942c9bba11ed
zhenyashumel <shumel02@mail.ru>
2017-05-22 21:26:35 +0300

    Create Class.h

---
+++ OOP_cpp/class_in_files/Class.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class Invoker1
+{
+	int Quas;
+	int Exort;
+
+public:
+
+	Invoker1();	
+	~Invoker1();
+	Invoker1(int x, int y);
+	int getExort()const;
+	
+	void setExort(const int a);
+	
+};
+
