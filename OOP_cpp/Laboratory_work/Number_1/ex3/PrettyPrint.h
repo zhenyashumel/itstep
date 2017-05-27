7bb364d6dbeecf10432021a45d71319e67f3eae8
zhenyashumel <shumel02@mail.ru>
2017-05-27 19:10:50 +0200

    Create PrettyPrint.h

---
+++ OOP_cpp/Laboratory_work/Number_1/ex3/PrettyPrint.h
@@ -0,0 +1,13 @@
+#pragma once
+#include<iostream>
+#include<string>
+
+class PrettyPrint
+{
+public:
+	PrettyPrint(){};
+	std::string concatenation(std::string, std::string)const;
+	std::string substitution(std::string);
+	std::string coup(std::string);
+
+};
