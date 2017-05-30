4fd6a7d2d7e86e865b6d4ebbe2956bb0d6ef516e
zhenyashumel <shumel02@mail.ru>
2017-05-30 09:28:10 +0200

    Update Prog.cpp

--- OOP_cpp/Laboratory_work/Number_1/ex3/Prog.cpp
+++ OOP_cpp/Laboratory_work/Number_1/ex3/Prog.cpp
@@ -1,15 +1,15 @@
 #include"PrettyPrint.h"
 int main()
 {
-	std::string a = "abcdefghijklmnopqrstuvwxyz",b="abc";
+	std::string a = "abcdefghijklmnopqrstuvwxyz", b = "abc";
 	PrettyPrint x;
-	std::cout << "Join two lines"<<std::endl;
-	std::cout << x.concatenation(a, b);	
+	std::cout << "Join two lines" << std::endl;
+	std::cout << x.concatenation(a, b) << std::endl;
 	std::cout << "We replace all the symbols a on o" << std::endl;
-	std::cout << x.substitution(a);
-	a = x.coup(a);
+	std::cout << x.substitution(a) << std::endl;
 	std::cout << "Flip the line" << std::endl;
+	x.coup(&a);
 	std::cout << a;
-	
+
 	return 0;
 }
