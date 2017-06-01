86c8648d5b6c7534b5d8290cd75d9a279a655ab8
zhenyashumel <shumel02@mail.ru>
2017-06-01 13:22:57 +0300

    Create Test.h

---
+++ OOP_cpp/Laboratory_work/Number_4/Test.h
@@ -0,0 +1,23 @@
+#include "MyVector.h"
+
+class Test {
+public:
+	void test() const;
+private:
+	const char* status_ok = "OK";
+	const char* status_error = "Error";
+
+	bool check_error(MyVector&, const int, const int) const;
+	bool test_ctor_1() const;
+	bool test_ctor_2() const;
+	bool test_copy_ctor_1() const;
+	bool test_copy_ctor_2() const;
+	bool test_at() const;
+	bool test_push_back() const;
+	bool test_pop_back() const;
+	bool test_ctor_8() const;
+	bool test_sort()const;
+
+	void getVector(MyVector) const;
+	MyVector createVector(const size_t) const;
+}; 
