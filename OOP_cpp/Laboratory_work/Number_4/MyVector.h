6da905759356dc757cf7204359a6b1e414e03855
zhenyashumel <shumel02@mail.ru>
2017-06-01 13:23:08 +0300

    Create MyVector.h

---
+++ OOP_cpp/Laboratory_work/Number_4/MyVector.h
@@ -0,0 +1,50 @@
+#include<iostream>
+#pragma once
+class MyVector
+{
+	double *arr;
+	unsigned int size;
+	unsigned int capacity = 32;
+	size_t counterVector = 0;
+	size_t counterMaxVector = 0;
+public:
+	MyVector() : capacity(32), size(0)
+	{
+		arr = new double[32];
+		++counterVector;
+		++counterMaxVector;
+
+	}
+	MyVector(double[], int);
+
+	MyVector(const MyVector& myObj);
+
+	MyVector(MyVector && myObj);
+
+
+	~MyVector()
+	{
+		delete[] arr;
+		counterVector-=1;
+	}
+	bool empty();
+	size_t get_size() const;
+	unsigned int get_capacity() const;
+	void push_back(double value);
+	double pop_back();
+	double& at(int index);
+	void print();
+	double get_min()const ;
+	double get_max()const ;
+	size_t getCounterVector()
+	{
+		return counterVector;
+	}
+
+	size_t getCounterMaxVector()
+	{
+		return counterMaxVector;
+	}
+	void sort();
+
+}; 
