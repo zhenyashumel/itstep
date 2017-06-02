beafb9bdb74de1cca719dab1dfa688617dfec052
zhenyashumel <shumel02@mail.ru>
2017-06-02 16:31:21 +0300

    Create Main.cpp

---
+++ OOP_cpp/Laboratory_work/Number_5/Main.cpp
@@ -0,0 +1,92 @@
+#include "Digit.h"
+
+void test()
+{
+	Digit a;
+	Digit b(13);
+	if ((a + b).getDigit() == b.getDigit() + a.getDigit())
+		std::cout << "The addition operator worked correctly !!!\n";
+	else
+	{
+		std::cout << "Oops... Something went wrong :(";
+		
+	}
+	a.setDigit(10);
+	if ((b-a).getDigit() == b.getDigit()-a.getDigit())
+		std::cout << "The subtraction operator worked correctly !!!\n";
+	else
+	{
+		std::cout << "Oops... Something went wrong :(";
+		
+	}
+	if(((a*b)/a).getDigit()==b.getDigit())
+		std::cout << "The operators of division and multiplication worked correctly!!!\n";
+	else
+	{
+		std::cout << "Oops... Something went wrong :(";
+		
+	}
+	if((b%a).getDigit()== b.getDigit() % a.getDigit())
+		std::cout << "The division operator modulo worked correctly!!!\n";
+	else
+	{
+		std::cout << "Oops... Something went wrong :(";
+		
+	}
+	if ((-a).getDigit() == a.getDigit()*-1)
+		std::cout << "The unary minus operator works correctly!!!\n";
+	else
+	{
+		std::cout << "Oops... Something went wrong :(";
+		
+	}
+	if (a<b&&b>a&&a <= b&&b >= a&&a != b)
+		std::cout << "Comparison operators work correctly!!!\n";
+	else
+	{
+		std::cout << "Oops... Something went wrong :(";
+		
+	}
+	a.setDigit(5);
+	b.setDigit(100);
+	if (a.getDigit() + 1 == (a++).getDigit()   && (b--).getDigit() == b.getDigit() - 1)
+		std::cout << "Postfix increment and decrement work correctly!!!\n";
+	else
+	{
+		std::cout << "Oops... Something went wrong :(";
+
+	}
+	int temp1=5, temp2=10;
+	a.setDigit(temp1);
+	b.setDigit(temp2);
+	++a; --b;
+	
+	if(a.getDigit()-temp1==1&& temp2-b.getDigit())
+		std::cout << "Prefix increment and decrement work correctly!!!\n";
+	else
+	{
+		
+		std::cout << "Oops... Something went wrong :(";
+
+	}
+	a = b;
+	if (a.getDigit() == b.getDigit())std::cout << "Assignment operator is working correctly!!!\n";
+	else
+	{
+
+		std::cout << "Oops... Something went wrong :(";
+
+	}
+
+}
+
+
+int main()
+{
+	std::cout << "Here we will overload the operators. Probably" << std::endl;
+	std::cout << "Press Enter to continue!!!";
+	std::cin.get();
+	test();
+
+	return 0;
+}
