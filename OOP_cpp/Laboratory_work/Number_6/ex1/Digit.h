4eca6b001ce7a0cb55434f28366868966de0ffb4
zhenyashumel <shumel02@mail.ru>
2017-06-06 10:27:47 +0300

    Create Digit.h

---
+++ OOP_cpp/Laboratory_work/Number_6/ex1/Digit.h
@@ -0,0 +1,113 @@
+#pragma once
+#include<iostream>
+
+class Digit
+{
+	int digit;
+	static size_t counter;
+
+public:
+
+	Digit() :digit(0)
+	{
+		++counter;
+	}
+
+	explicit Digit(int x) :digit(x)
+	{
+		++counter;
+	}
+
+	Digit(double x) :digit((int)x)
+	{
+		++counter;
+	}
+
+	Digit(const Digit &m1) :digit(m1.digit) { ++counter; }
+
+	Digit(Digit &&m1) :digit(m1.digit)
+	{
+		m1.digit = 0;
+		++counter;
+
+	}
+
+	~Digit()
+	{
+		--counter;
+	}
+
+	operator int()
+	{
+		return digit;
+	}
+
+	operator double()
+	{
+		return (double)digit;
+	}
+
+	int getDigit()
+	{
+		return digit;
+	}
+
+	void setDigit(int value)
+	{
+		digit = value;
+	}
+
+	static size_t getCounter()
+	{
+		return counter;
+	}
+
+	Digit operator-()
+	{
+		return -digit;
+	}
+
+	void operator()(int value)
+	{
+		digit = value;
+	}
+
+	Digit operator+(const Digit newDigit);
+	Digit operator+(int value);
+	Digit operator-(const Digit newDigit);
+	Digit operator-(int value);
+	Digit operator*(const Digit newDigit);
+	Digit operator*(int value);
+	Digit operator/(const Digit newDigit);
+	Digit operator/(int value);
+	Digit operator%(const Digit newDigit);
+	Digit operator%(int value);
+
+	bool operator >(const Digit newDigit);
+	bool operator <(const Digit newDigit);
+	bool operator ==(const Digit newDigit);
+	bool operator !=(const Digit newDigit);
+	bool operator >=(const Digit newDigit);
+	bool operator <=(const Digit newDigit);
+
+	Digit operator++(int);
+	Digit operator--(int);
+	Digit& operator++();
+	Digit& operator--();
+
+	Digit &operator=(const Digit &newDigit);
+	Digit &operator=(Digit &&newDigit);
+
+	Digit& operator+=(const Digit &newDigit);
+	Digit& operator-=(const Digit &newDigit);
+	Digit& operator*=(const Digit &newDigit);
+	Digit& operator/=(const Digit &newDigit);
+	Digit& operator%=(const Digit &newDigit);
+
+
+
+
+
+
+
+};
