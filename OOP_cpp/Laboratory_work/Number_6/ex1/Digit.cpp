7cab46bbf4ac3122bd1876966302cffb7434dacb
zhenyashumel <shumel02@mail.ru>
2017-06-06 10:28:05 +0300

    Create Digit.cpp

---
+++ OOP_cpp/Laboratory_work/Number_6/ex1/Digit.cpp
@@ -0,0 +1,181 @@
+
+#include"Digit.h"
+size_t Digit::counter = 0;
+Digit Digit::operator+(const Digit newDigit)
+{
+	Digit temp(digit);
+	temp.operator+=(newDigit.digit);
+	return temp;
+}
+
+Digit Digit::operator+(int value)
+{
+	Digit temp(digit);	
+	temp.operator+=(value);
+	return temp;
+}
+
+Digit Digit:: operator-(const Digit newDigit)
+{
+	Digit temp(digit);
+	temp.operator-=(newDigit.digit);
+	return temp;
+}
+
+Digit Digit::operator-(int value)
+{
+	Digit temp(digit);
+	temp.operator-=(value);
+	return temp;
+}
+
+Digit Digit::operator*(const Digit newDigit)
+{
+	Digit temp(digit);
+	temp.operator*=(newDigit.digit);
+	return temp;
+}
+
+Digit Digit::operator*(int value)
+{
+	Digit temp(digit);
+	temp.operator*=(value);
+	return temp;
+}
+
+Digit Digit::operator/(const Digit newDigit)
+{
+	Digit temp(digit);
+	temp.operator/=(newDigit.digit);
+	return temp;
+}
+
+Digit Digit::operator/(int value)
+{
+	Digit temp(digit);
+	temp.operator/=(value);
+	return temp;
+}
+
+Digit Digit::operator%(const Digit newDigit)
+{
+	Digit temp(digit);
+	temp.operator%=(newDigit.digit);
+	return temp;
+}
+
+Digit Digit::operator%(int value)
+{
+	Digit temp(digit);
+	temp.operator%=(value);
+	return temp;
+}
+
+
+bool Digit:: operator >(const Digit newDigit)
+{
+	return digit > newDigit.digit;
+}
+
+bool Digit:: operator <(const Digit newDigit)
+{
+	return digit < newDigit.digit;
+}
+
+bool Digit::operator ==(const Digit newDigit)
+{
+	return digit == newDigit.digit;
+}
+
+bool Digit::operator !=(const Digit newDigit)
+{
+	return digit != newDigit.digit;
+}
+
+bool Digit:: operator >=(const Digit newDigit)
+{
+	return digit >= newDigit.digit;
+}
+
+bool Digit::operator <=(const Digit newDigit)
+{
+	return digit <= newDigit.digit;
+}
+
+
+
+Digit Digit::operator++(int)
+{
+	Digit tmp = *this;
+	tmp.digit++;
+	return tmp;
+
+}
+
+Digit Digit::operator--(int)
+{
+	Digit tmp = *this;
+	tmp.digit--;
+	return tmp;
+}
+
+Digit& Digit::operator++()
+{
+	++digit;
+	return *this;
+
+}
+
+Digit& Digit::operator--()
+{
+	--digit;
+	return *this;
+}
+
+
+Digit& Digit::operator=(const Digit& newDigit)
+{
+	if (this == &newDigit)
+		return *this;
+	digit = newDigit.digit;
+	return *this;
+}
+
+Digit& Digit::operator=(Digit&& newDigit)
+{
+	if (this == &newDigit)
+		return *this;
+	digit = newDigit.digit;
+	newDigit.digit = 0;
+	return *this;
+}
+
+Digit& Digit::operator+=(const Digit &newDigit)
+{
+	digit += newDigit.digit;
+	return *this;
+}
+
+Digit& Digit::operator-=(const Digit &newDigit)
+{
+	digit -= newDigit.digit;
+	return *this;
+}
+
+Digit& Digit::operator*=(const Digit &newDigit)
+{
+	digit *= newDigit.digit;
+	return *this;
+}
+
+Digit& Digit::operator/=(const Digit &newDigit)
+{
+	digit /= newDigit.digit;
+	return *this;
+}
+
+Digit& Digit::operator%=(const Digit &newDigit)
+{
+	digit %= newDigit.digit;
+	return *this;
+}
