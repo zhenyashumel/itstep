85c5dc23da318bd661f383e389566aa21d7f9ab3
zhenyashumel <shumel02@mail.ru>
2017-06-24 14:40:00 +0300

    Create Dictionary.h

---
+++ OOP_cpp/Home_Work/Number_1/ex1/Dictionary.h
@@ -0,0 +1,25 @@
+#pragma once
+#include<iostream>
+#include<map>
+#include<fstream>
+#include<vector>
+#include<string>
+
+
+class Dictionary
+{
+
+	std::map<std::string, std::vector<std::string> > dict;
+	std::string out = ""; //здесь хранятся результаты всех операций , которые(при желании) будем записывать в файл 
+
+public:
+	Dictionary() = delete;
+	Dictionary(std::string);
+	~Dictionary() = default;
+
+	void print();
+	void bestWord();
+	void input(std::string);
+	void write();
+	
+};
