31f0ecff8cb663bf5cf89d3cd64e4260080f0664
zhenyashumel <shumel02@mail.ru>
2017-07-20 15:21:33 +0300

    Update Recipes.h

--- OOP_cpp/Exam/Hard/Recipes.h
+++ OOP_cpp/Exam/Hard/Recipes.h
@@ -8,15 +8,18 @@ class Recipes
 public:
 	std::map < std::string, std::map < std::string, int>> book;
 	std::vector<std::string> last_recipe;
+	std::string last_word;
+	std::vector<std::string> often;
 
 public:
 
 	Recipes();
 	~Recipes() = default;
 
-	void recipe_generation();
+	void recipe_generation(const int quantity);
 	void print_recipe() const;
 	void finput_recipe() const;
+	void several_recipes(const int quantity, int count);
 
 	void menu();
 };
