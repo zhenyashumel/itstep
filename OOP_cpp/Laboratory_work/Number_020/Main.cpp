78f4bf66f31bc89ad3516f0c421fd951289abf3c
zhenyashumel <shumel02@mail.ru>
2017-08-12 12:12:29 +0300

    Create Main.cpp

---
+++ OOP_cpp/Laboratory_work/Number_020/Main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include "Student.h"
+#include"RecordBook.h"
+
+int main()
+{
+
+	Student obj("Zhenya", "Shumel", "Valerievich", 20, 9, 2002, 2, "B31016");
+	
+	obj.getInfo();
+	obj.printMarks();
+
+	
+	
+	
+}
