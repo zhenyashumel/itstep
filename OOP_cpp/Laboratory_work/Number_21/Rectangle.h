0430786134633e51d04a6e41ee860c057c0c25ff
zhenyashumel <shumel02@mail.ru>
2017-08-13 14:05:37 +0300

    Create Rectangle.h

---
+++ OOP_cpp/Laboratory_work/Number_21/Rectangle.h
@@ -0,0 +1,14 @@
+#pragma once
+#include"Parallelogram.h"
+class Rectangle : public Parallelogram
+{
+
+public:
+	Rectangle(const double, const double);
+	virtual ~Rectangle() {};
+
+	virtual double getArea() override;
+
+
+};
+
