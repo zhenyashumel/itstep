0c47091c7ee80edd23e78ca2e8a4d8e8327aafab
zhenyashumel <shumel02@mail.ru>
2017-08-17 12:39:25 +0300

    Update Dictionary.cpp

--- OOP_cpp/Home_Work/Number_1/ex1/Dictionary.cpp
+++ OOP_cpp/Home_Work/Number_1/ex1/Dictionary.cpp
@@ -12,21 +12,21 @@ Dictionary::Dictionary(std::string fname)
 
 	std::cout << "Connect!!! Read the dictionary..." << std::endl;
 
-	std::string rank;
 	std::string word;
-	std::string count;
-
-	while (in >> rank)
+	while (in >> word)
 	{
-		in >> word;
-		in >> count;
 		
-		std::vector<std::string> data;
-		data.push_back(word);
-		data.push_back(count);
+		if (word[word.length() - 1] == ',' || word[word.length() - 1] == '.' || word[word.length() - 1] == ':' || word[word.length() - 1] == ';')
+			word.erase(word.length() - 1);
+		
+		if (isupper(word[0]))
+			word[0] = tolower(word[0]);
 
-		dict.insert({ rank, data });
-			
+		if (dict.count(word) == 0)
+			dict.insert({ word, 1 });
+		else
+			dict[word] = dict[word] + 1;	
+
 	}
 
 }
@@ -35,50 +35,26 @@ void Dictionary::print()
 {
 	for (auto el : dict)
 	{
-		std::cout << el.first << "  " << el.second[0] << "  " << el.second[1] << std::endl;
-		out += el.first + "  " + el.second[0] + "  " + el.second[1] + '\n';
+		std::cout << el.first << "  " << el.second << std::endl;
+		out += el.first + "  " + char(el.second) + '\n';
 	}
 	out += '\n';
 }
 
 void Dictionary::bestWord()
 {
-	auto it = dict.find("1");
-	std::cout << "Word: " << it->second[0]<<std::endl
-		<<"Frequency: "<<it->second[1];
+	std::pair<std::string, int> max = *dict.begin();
 
-	out += "Word: " + it->second[0] + '\n' + "Frequency: " + it->second[1] + '\n';
-}
-
-void Dictionary::input(std::string fname)
-{
-	std::ifstream in(fname);
-
-	if (!in.is_open())
+	for (auto el : dict)
 	{
-		std::cout << "Error connecting to file!!!\n";
-		exit(1);
+		if (el.second > max.second)
+			max = el;
 	}
 
-	std::cout << "Connect!!! Read the dictionary..." << std::endl;
-
-	std::string rank;
-	std::string word;
-	std::string count;
-
-	while (in >> rank)
-	{
-		in >> word;
-		in >> count;
-
-		std::vector<std::string> data;
-		data.push_back(word);
-		data.push_back(count);
-
-		dict.insert({ rank, data });
-
-	}
+	std::cout << max.first << " " << max.second;
 }
+
+
 
 void Dictionary::write()
 {
