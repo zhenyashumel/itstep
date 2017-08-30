88ac23364e97717af3cc11a58d05ca33a0b4b9b5
zhenyashumel <shumel02@mail.ru>
2017-08-30 11:08:53 +0300

    Add files via upload

---
+++ WinAPI/Laboratory_Work/Work_05/ex2/Win32Project9/Win32Project9/resource.h
@@ -0,0 +1,33 @@
+//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется Win32Project9.rc
+//
+#define WindowMain                      101
+#define IDB_BITMAP1                     103
+#define IDB_BITMAP2                     104
+#define IDB_BITMAP3                     105
+#define IDB_BITMAP4                     106
+#define IDB_BITMAP5                     107
+#define IDB_BITMAP6                     108
+#define IDB_BITMAP7                     109
+#define IDB_BITMAP8                     110
+#define IDB_BITMAP9                     111
+#define IDB_BITMAP10                    112
+#define Image                           1001
+#define ButtonStart                     1002
+#define ButtonStop                      1003
+#define ButtonCat                       1004
+#define ButtonDog                       1005
+#define ButtonFast                      1006
+#define ButtonSlow                      1007
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        113
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
