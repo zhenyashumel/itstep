c79c84bda47d9a173b79db57fd13627b2b864758
zhenyashumel <shumel02@mail.ru>
2017-08-30 19:09:23 +0300

    Create CreateDialog.cpp

---
+++ WinAPI/CreateDialog.cpp
@@ -0,0 +1,42 @@
+#include<Windows.h>
+#include"resource.h"
+
+BOOL CALLBACK DlgProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+
+	HWND hWnd = CreateDialog(hInstance, MAKEINTRESOURCE(WindowMain), NULL, DlgProc);
+	ShowWindow(hWnd, true);
+
+	MSG iMsg;
+	while (GetMessage(&iMsg, NULL, NULL, 0))
+	{
+		TranslateMessage(&iMsg);
+		DispatchMessage(&iMsg);
+		if (iMsg.message == WM_SYSKEYDOWN || iMsg.message == WM_CHAR)
+		{
+			SendMessage(hWnd, iMsg.message, iMsg.wParam, iMsg.lParam);
+		}
+	}
+	return iMsg.wParam;
+
+}
+
+
+
+BOOL CALLBACK DlgProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+
+	switch (iMsg)
+	{
+	case WM_INITDIALOG:
+		return true;
+
+	case WM_CLOSE:
+		DestroyWindow(hWnd);
+		PostQuitMessage(0);
+		return true;
+	}
+	return false;
+}
