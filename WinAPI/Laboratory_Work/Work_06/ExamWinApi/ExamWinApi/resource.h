50e732358dd95381226fd996575ea35e679a32ee
zhenyashumel <shumel02@mail.ru>
2017-09-06 16:24:00 +0300

    Add files via upload

---
+++ WinAPI/Laboratory_Work/Work_06/ExamWinApi/ExamWinApi/resource.h
@@ -0,0 +1,42 @@
+//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется ExamWinApi.rc
+//
+#define IDD_MAINDLG                     101
+#define Authorization                   103
+#define EDIT_START                      1001
+#define ButtonAuto                      1003
+#define ButtonOpen                      1004
+#define ButtonClear                     1005
+#define ButtonCopy                      1006
+#define ButtonCopyAll                   1007
+#define EditSecond                      1008
+#define ButtonClear2                    1009
+#define ButtonWrite                     1011
+#define LogEdit                         1013
+#define PassEdit                        1014
+#define ButtonLogin                     1015
+#define ButtonCancel                    1016
+#define List                            1017
+#define ButtonAddList                   1018
+#define DeleteList                      1019
+#define ClearList                       1020
+#define WriteList                       1021
+#define Count                           1022
+#define text                            1023
+#define Combo                           1024
+#define EditInfo                        1025
+#define ButtonSave                      1026
+#define IDC_BUTTON2                     1027
+#define ButtonWriteCombo                1027
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1028
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
