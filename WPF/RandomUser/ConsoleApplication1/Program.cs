f9f3b348db796ff3951702f3de19bef8c0e32634
zhenyashumel <shumel02@mail.ru>
2017-12-19 19:49:47 +0300

    Add files via upload

---
+++ WPF/RandomUser/ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using MVVMLight_Sample.Helper.JSONClasses;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var request = WebRequest.Create(@"https://randomuser.me/api/?results=10"); ;
+            WebResponse response = request.GetResponse();
+
+            var dataStream = response.GetResponseStream();
+
+            StreamReader reader = new StreamReader(dataStream);
+
+            string responseFromServer = reader.ReadToEnd();
+
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+            var users =  (Rootobject)ser.Deserialize(responseFromServer, typeof(Rootobject));
+
+
+            Console.WriteLine(users);
+            Console.Read();
+        }
+    }
+}
